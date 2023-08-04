3d929e0506e9d2f67529c1034bdcfb3ad4a710c6
Francisco Ravasco <fsravasco@gmail.com>
2023-08-04 14:17:12 +0100

    Solves first 2 exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(text,nTimes) {
+    stringJoin = ""
+    if (nTimes<0){
+        return "ERROR"
+    }else{
+        for (i=0; i<nTimes;i++){
+            stringJoin = stringJoin+text
+        }
+    }
+    return stringJoin
 };
 
 // Do not edit below this line
